181f820600f35c9c11dbec9a12cbd87274c94470
meng <1732430142@qq.com>
2024-08-16 23:24:14 +0800

    绝对任务延迟

--- src/main.cpp
+++ src/main.cpp
@@ -1,119 +1,56 @@
 #include <Arduino.h>
 
-// 养成良好习惯，被多进程和中断调用的变量使用 volatile 修饰符
-volatile uint32_t inventory = 100; //总库存
-volatile uint32_t retailCount = 0; //线下销售量
-volatile uint32_t onlineCount = 0; //线上销售量
+/*
+  程序： 绝对任务延迟
+  公众号：孤独的二进制
+  API：
+    vTaskDelayUntil(&xLastWakeTime, xFrequency)
+      最后一次的唤醒时间是指针类型。
+      本函数会自动更新xLastWakeTime为最后一次唤醒的时间
+      所以无需手动在while循环内对其手动赋值
+    xTaskGetTickCount()
+      Tick Coun 和 Arduino Millis一样
+      uint32_t类型 49天后overflow
+*/
 
-SemaphoreHandle_t xMutexInventory = NULL; //创建信号量Handler
+void showStockTask(void *ptParam) {
+  static float stockPrice = 99.57; //股票价格
 
-TickType_t timeOut = 1000; //用于获取信号量的Timeout 1000 ticks
+  //最后一次唤醒的tick count，第一次使用需要赋值
+  //以后此变量会由vTaskDelayUntil自动更新
+  TickType_t xLastWakeTime = xTaskGetTickCount();
 
+  const TickType_t xFrequency = 3000; // 间隔 3000 ticks = 3 seconds
 
-void retailTask(void *pvParam) {
-  while (1) {
+  for (;;) {
+    //恰恰算算，经过思考，既然我们叫做LastWakeTime，那么 vTaskDelayUntil 应该放在循环的第一句话
+    //如果放在循环的最后一句话，应该改为xLastSleepTime 才更加合适
+    // 看懂的朋友， 请鼓掌
+    // 哦，我无法听到掌声，干脆帮我按住 点赞三秒 对我的视频进行强力推荐吧
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
 
-    // 在timeout的时间内如果能够获取就继续
-    // 通俗一些：获取钥匙
-    if (xSemaphoreTake(xMutexInventory, timeOut) == pdPASS) {
-      //被MUTEX保护的内容叫做 Critical Section
+    //验证当前唤醒的时刻tick count
+    Serial.println(xTaskGetTickCount());
+    //验证xLastWake Time是否被vTaskDelayUntil更新
+    //Serial.println(xLastWakeTime);
 
+    // ------- 很复杂的交易股票计算，时间不定 ---------
+    stockPrice = stockPrice * (1 + random(1, 20) / 100.0); vTaskDelay(random(500, 2000));
 
-      //以下实现了带有随机延迟的 inventory减1；
-      //等效为 inventory--； retailCount++；
-      uint32_t inv = inventory;
-      for (int i; i < random(10, 100); i++) vTaskDelay(pdMS_TO_TICKS(i));
-      if (inventory > 0) {
-        inventory = inv - 1;
-        retailCount++;
+    Serial.print("Stock Price : $");
+    Serial.println(stockPrice);
 
-        //释放钥匙
-        xSemaphoreGive(xMutexInventory);
-      } else {
-        //无法获取钥匙
-      }
-
-
-    };
-
-    vTaskDelay(100); //老板要求慢一些，客户升级后，可以再加快速度
+    //使用vTaskDelay试试看会如何
+    //vTaskDelay(xFrequency);
   }
-}
 
-void onlineTask(void *pvParam) {
-  while (1) {
-
-    // 在timeout的时间内如果能够获取二进制信号量就继续
-    // 通俗一些：获取钥匙
-    if (xSemaphoreTake(xMutexInventory, timeOut) == pdPASS) {
-      //被MUTEX保护的内容叫做 Critical Section
-      //以下实现了带有随机延迟的 inventory减1；
-      //等效为 inventory--； retailCount++；
-      uint32_t inv = inventory;
-      for (int i; i < random(10, 100); i++) vTaskDelay(pdMS_TO_TICKS(i));
-      if (inventory > 0) {
-        inventory = inv - 1;
-        onlineCount++;
-
-        //释放钥匙
-        xSemaphoreGive(xMutexInventory);
-      } else {
-        //无法获取钥匙
-      }
-    };
-
-    vTaskDelay(100); //老板要求慢一些，客户升级后，可以再加快速度
-  }
-}
-
-
-void showTask(void *pvParam) {
-  while (1) {
-
-    printf("Inventory : %d\n", inventory);
-    printf("  Retail : %d, Online : %d\n", retailCount, onlineCount);
-
-
-    if (inventory == 0 ) {
-      uint32_t totalSales = retailCount + onlineCount;
-      printf("-----SALES SUMMARY-----\n");
-      printf("  Total Sales:  %d\n", totalSales);
-      printf("  OverSales:  %d\n", 100 - totalSales);
-    }
-    vTaskDelay(pdMS_TO_TICKS(1000));
-  }
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-
-  xMutexInventory = xSemaphoreCreateMutex(); //创建MUTEX
-
-  if (xMutexInventory == NULL) {
-    printf("No Enough Ram, Unable to Create Semaphore.");
-  } else {
-    xTaskCreate(onlineTask,
-                "Online Channel",
-                1024 * 4,
-                NULL,
-                1,
-                NULL);
-    xTaskCreate(retailTask,
-                "Retail Channel",
-                1024 * 4,
-                NULL,
-                1,
-                NULL);
-    xTaskCreate(showTask,
-                "Display Inventory",
-                1024 * 4,
-                NULL,
-                1,
-                NULL);
-  }
-
+  xTaskCreate(showStockTask, "Show Stock Price", 1024 * 6, NULL, 1, NULL);
 }
 
 void loop() {
+
 }
