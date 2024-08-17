997c9ce4b582b09b7da9526875423d29d5c2a239
meng <1732430142@qq.com>
2024-08-17 15:01:55 +0800

    任务管理创建，删除，暂停，恢复

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,122 @@
 #include <Arduino.h>
 /*
-  程序： 内存管理
+  程序： 任务管理
   公众号：孤独的二进制
   API：
-    ESP.getHeapSize() //本程序Heap最大尺寸
-    ESP.getFreeHeap() //当前Free Heap最大尺寸
-    uxTaskGetStackHighWaterMark(taskHandle) //Task内存使用最大水位线，内存是水
-     
-    What is the Highest Water Mark?
-    the minimum amount of remaining stack space that was available to the task 
-    since the task started executing - that is the amount of stack that remained 
-    unused when the task stack was at its greatest (deepest) value. This is what 
-    is referred to as the stack 'high water mark'.
+    BaseType_t xTaskCreate(,,,,,); //任务创建
+    void vTaskDelete( TaskHandle_t xTask ); //任务删除 
+    void vTaskSuspend( TaskHandle_t xTaskToSuspend ); //任务暂停
+    void vTaskResume( TaskHandle_t xTaskToResume ); //任务恢复
 */
-TaskHandle_t taskHandle;
-int taskMem = 1024;
+//#include <LiquidCrystal_I2C.h>
+//LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-void task(void *ptParam) {
-  //volatile char hello[1000] = {0}; //必须要用volatile修饰语，否则会被编译器优化掉
+TaskHandle_t biliHandle = NULL; //Task Handler
+
+void control(void *ptParam) {  //按钮控制
+  
+  pinMode(32, INPUT_PULLUP);
+  pinMode(33, INPUT_PULLUP);
+  pinMode(25, INPUT_PULLUP);
+  pinMode(26, INPUT_PULLUP);
+
   while (1) {
+    // 创建任务
+    if (digitalRead(32) == LOW) {
+      //判断是否之前已经创建了Bilibili channel task， 如果没有创建，则创建该Task
+      if (biliHandle == NULL) {
+        if (xTaskCreate(radioBilibili, "Bilibili Channel", 1024 * 8, NULL, 1, &biliHandle) == pdPASS) {
+          Serial.print(xTaskGetTickCount());
+          Serial.println(" - LOG: Task is Created.");
+        } else {
+          Serial.print(xTaskGetTickCount());
+          Serial.println(" - WARNING: Unable to Create Task.");
+        }
+      } else {
+        Serial.print(xTaskGetTickCount());
+        Serial.println(" - WARNING: Task **WAS** Created.");
+      }
+      vTaskDelay(120); //粗暴的Button Debounce
+    }
 
-    //不推荐在task中执行，因为Serial.print也会消耗内存
-    // vTaskDelay(2000);
-    // int waterMark = uxTaskGetStackHighWaterMark(nullptr);
-    // Serial.print("Task Free Memory: ");
-    // Serial.print(waterMark);
-    // Serial.println(" Bytes");
-    // Serial.print("Task Used Memory: ");
-    // Serial.print(taskMem - waterMark);
-    // Serial.println(" Bytes");
-    // Serial.println("");
+    //任务删除
+    if (digitalRead(33) == LOW) {
+      //注意在删除任务前，一定要确保任务是存在的
+      //删除不存在的任务，比如连续删除两次，自动重启
+      if (biliHandle != NULL) {
+        vTaskDelete( biliHandle );
+        lcdClear(); //清空LCD
+        biliHandle = NULL; //手动将handler设置为空
+      }
 
+      if (biliHandle != NULL) {
+        Serial.print(xTaskGetTickCount());
+        Serial.println(" - WARNING: Unable to Delete Task.");
+      } else {
+        Serial.print(xTaskGetTickCount());
+        Serial.println(" - LOG: Task is Deleted.");
+      }
+      vTaskDelay(120);  //粗暴的Button Debounce
+    }
+
+    // 任务暂停
+    if (digitalRead(25) == LOW) {
+      if (biliHandle != NULL) {
+        vTaskSuspend(biliHandle);
+        Serial.print(xTaskGetTickCount());
+        Serial.println(" - LOG: Task is suspended.");
+      } else {
+        Serial.print(xTaskGetTickCount());
+        Serial.println(" - WARNING: Unable to Suspend Task.");
+      }
+      vTaskDelay(120);  //粗暴的Button Debounce
+    }
+
+    // 任务恢复
+    if (digitalRead(26) == LOW) { 
+      if (biliHandle != NULL) {
+        vTaskResume(biliHandle);
+        Serial.print(xTaskGetTickCount());
+        Serial.println(" - LOG: Task is resumed.");
+      } else {
+        Serial.print(xTaskGetTickCount());
+        Serial.println(" - WARNING: Unable to Resume Task.");
+      }
+      vTaskDelay(120);  //粗暴的Button Debounce
+    }
   }
 }
-void setup() {
+
+void radioBilibili(void *ptParam) {  //任务主体
+
+  // lcd.init();
+  // lcd.backlight();
+  // lcd.setCursor(0, 0);
+  // lcd.print(" Bilibili   Channel ");
+  // lcd.setCursor(0, 1);
+  // lcd.print("-FreeRTOS on EPS32- ");
+  // lcd.setCursor(0, 2);
+  // lcd.print("Study Hard  &  Smart");
+
+  while (1) {
+    // lcd.setCursor(9, 3);
+    // lcd.print(xTaskGetTickCount() / 100);
+    vTaskDelay(100);
+  }
+}
+
+void lcdClear() { //清空LCD
+  // lcd.clear();
+  // lcd.noBacklight();
+  // lcd.setCursor(0, 0);
+  // lcd.print("                                                                                                    ");
+}
+
+void setup()
+{
   Serial.begin(115200);
-
-  int heapSize = ESP.getHeapSize();
-  Serial.print("Total Heap Size:  ");
-  Serial.print(heapSize);
-  Serial.println(" Bytes");
-
-  int heapFree = ESP.getFreeHeap();
-  Serial.print("Free Heap Size:  ");
-  Serial.print(heapFree);
-  Serial.println(" Bytes");
-  Serial.println("");
-
-
-  Serial.println("Create Task ...");
-  xTaskCreate(task, "", taskMem, NULL, 1, &taskHandle);
-
-  Serial.print("Free Heap Size:  ");
-  Serial.print(ESP.getFreeHeap());
-  Serial.println(" Bytes");
-  Serial.println("");
-
-  vTaskDelay(2000);
-  int waterMark = uxTaskGetStackHighWaterMark(taskHandle);
-  Serial.print("Task Free Memory: ");
-  Serial.print(waterMark);
-  Serial.println(" Bytes");
-  Serial.print("Task Used Memory: ");
-  Serial.print(taskMem - waterMark);
-  Serial.println(" Bytes");
-
+  xTaskCreate(control, "control panel", 1024 * 8, NULL, 1, NULL);
 }
 
-void loop() {
 
-}
+void loop() {}
