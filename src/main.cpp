e9a9152d8f5354a5e8cea53b0fb6120ea3292597
meng <1732430142@qq.com>
2024-08-17 16:16:41 +0800

    计数信号量

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,55 @@
 #include <Arduino.h>
 /*
-  程序：  Binary Semaphore 是一种信号机制 在中断中的用法
-         一个任务(生产者)发出信号。另外一个任务(消费者)接受信号
+  程序：  Counting Semaphore//计数信号量//可以用在OLED显示缓存
   公众号：孤独的二进制
 
-  语法：
-  SemaphoreHandle_t xHandler; 创建Handler
-  xHandler = xSemaphoreCreateBinary(); 创建一个二进制信号量 返回NULL，或者handler
-  xSemaphoreGive(xHandler); 生产者+1
-  xSemaphoreTake(xHanlder, timeout); 消费者-1 返回pdPASS, 或者pdFAIL
-
-  二进制信号量可以想成就是一个整数 0 或者 1
-  Give就是+1
-  Take就是-1
-
-  Take的时候如果这个整数是0的话，就等待一直到timeout
+  Counting Semphore时间上是一个内存中的整数，从0 - MAX
+  Give +1
+  Take -1
 */
+SemaphoreHandle_t xSemaPhone = NULL;
 
-volatile bool btnPressed = true;
-volatile bool btnReleased = false;
-volatile int counter = 0;
+String consumerA = "JD";
+String consumerB = "TMALL";
+String consumerC = "PDD";
 
-SemaphoreHandle_t xSemaLED = NULL; //创建信号量Handler
-volatile TickType_t btnDeounce = 0; //用于button Debounce
 
-TickType_t timeOut = 1000; //用于获取信号量的Timeout 1000 ticks
-
-void flashLED(void *pvParam) {
-
-  pinMode(23, OUTPUT);
+void producer(void *paParam) { //制造者 give
   while (1) {
-    if (xSemaphoreTake( xSemaLED, timeOut) == pdTRUE )
-    {
-      if ((xTaskGetTickCount() - btnDeounce) < 100) { //用于button debounce
-        digitalWrite(23, !digitalRead(23));
-        vTaskDelay(1000);
-      }
-    }
-  }
-}
 
-void readBtn(void *pvParam) {
-
-  pinMode(22, INPUT_PULLUP);
-
-  while (1) {
-    if (digitalRead(22) == LOW) {
-      xSemaphoreGive(xSemaLED);
-    }
+    for (int i = 0; i < random(100, 200); i++) vTaskDelay(10);
+    xSemaphoreGive(xSemaPhone);
+    Serial.println("...... 手机再放出一台,");
   }
 }
 
 
-void IRAM_ATTR ISR() {
-  btnDeounce = xTaskGetTickCountFromISR();
-  xSemaphoreGiveFromISR(xSemaLED, NULL);
+void consumer(void *pvParam) { //消费者 take
+  String website = *(String *)pvParam;
 
+  while (1) {
+    if (xSemaphoreTake(xSemaPhone, portMAX_DELAY) == pdTRUE ) {
+
+      for (int i = 0; i < random(200, 400); i++) vTaskDelay(10);
+      Serial.print(website);
+      Serial.println("抢到并销售一台: ");
+
+    }
+  }
 }
+
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  xSemaPhone = xSemaphoreCreateCounting(3, 0);
 
-  xSemaLED = xSemaphoreCreateBinary(); //创建二进制信号量
+  xTaskCreate(consumer, "consumer a", 1024 * 6, (void *)&consumerA, 1, NULL);
+  xTaskCreate(consumer, "consumer b", 1024 * 6, (void *)&consumerB, 1, NULL);
+  xTaskCreate(consumer, "consumer c", 1024 * 6, (void *)&consumerC, 1, NULL);
 
-  if (xSemaLED == NULL) {
-    printf("No Enough Ram, Unable to Create Semaphore.");
-  } else {
-    xTaskCreate(flashLED,
-                "Flash LED",
-                1024 * 4,
-                NULL,
-                1,
-                NULL);
-    // xTaskCreate(readBtn,
-    //             "Read Button",
-    //             1024 * 4,
-    //             NULL,
-    //             1,
-    //             NULL);
-  }
+  xTaskCreate(producer, "producer", 1024 * 6, NULL, 1, NULL);
 
-  pinMode(22, INPUT_PULLUP);
-  attachInterrupt(22, ISR, FALLING);
 }
-
-
-
 void loop() {
 }
 
