e4c6a701ab982e460ad3efe499db509d19ffc12d
meng <1732430142@qq.com>
2024-08-30 12:53:33 +0800

    BH17500基于RTOS

--- src/main.cpp
+++ src/main.cpp
@@ -12,60 +12,30 @@
 
 */
 BH1750 lightMeter(0x23);
-
-void setup() {
-
+volatile float lux = 0;
+void lightTask(void *parameter) {
   Serial.begin(9600);
 
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
   Wire.begin();
   // On esp8266 you can select SCL and SDA pins using Wire.begin(D4, D3);
-
-  /*
-
-    BH1750 has six different measurement modes. They are divided in two groups;
-    continuous and one-time measurements. In continuous mode, sensor
-    continuously measures lightness value. In one-time mode the sensor makes
-    only one measurement and then goes into Power Down mode.
-
-    Each mode, has three different precisions:
-
-      - Low Resolution Mode - (4 lx precision, 16ms measurement time)
-      - High Resolution Mode - (1 lx precision, 120ms measurement time)
-      - High Resolution Mode 2 - (0.5 lx precision, 120ms measurement time)
-
-    By default, the library uses Continuous High Resolution Mode, but you can
-    set any other mode, by passing it to BH1750.begin() or BH1750.configure()
-    functions.
-
-    [!] Remember, if you use One-Time mode, your sensor will go to Power Down
-    mode each time, when it completes a measurement and you've read it.
-
-    Full mode list:
-
-      BH1750_CONTINUOUS_LOW_RES_MODE
-      BH1750_CONTINUOUS_HIGH_RES_MODE (default)
-      BH1750_CONTINUOUS_HIGH_RES_MODE_2
-
-      BH1750_ONE_TIME_LOW_RES_MODE
-      BH1750_ONE_TIME_HIGH_RES_MODE
-      BH1750_ONE_TIME_HIGH_RES_MODE_2
-
-  */
-
-  // begin returns a boolean that can be used to detect setup problems.
   if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
     Serial.println(F("BH1750 Advanced begin"));
   } else {
     Serial.println(F("Error initialising BH1750"));
   }
+  while (true) {
+    // After setting up the sensor, you should wait a bit before
+    // using it, or else you'll get a zero reading
+    if (lightMeter.measurementReady()) {
+      lux = lightMeter.readLightLevel();
+    }
+    vTaskDelay(200);
+  }
+}
+void setup() {
+  xTaskCreatePinnedToCore(lightTask, "lightTask", 1024 * 8, NULL, 1, NULL, 1);
 }
 
 void loop() {
-  if (lightMeter.measurementReady()) {
-    float lux = lightMeter.readLightLevel();
-    Serial.print("Light: ");
-    Serial.print(lux);
-    Serial.println(" lx");
-  }
 }
