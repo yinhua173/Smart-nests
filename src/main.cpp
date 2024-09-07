f9a45dcbbbe989d11d5dff03d4b2e3d03a9ae917
meng <1732430142@qq.com>
2024-09-07 08:33:46 +0800

    删除setup函数创建的空闲任务

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 void setup() {
   xTaskCreatePinnedToCore(smokeTask, "lightTask", 1024 * 8, NULL, 1, NULL, 1);
   xTaskCreatePinnedToCore(BH1750Task, "BH1750Task", 1024 * 8, NULL, 1, NULL, 1);
-  xTaskCreate(bme680Task, "BME680", 1024 * 8, NULL, 1, NULL);
+  xTaskCreatePinnedToCore(bme680Task, "BME680", 1024 * 8, NULL, 1, NULL,1);
   vTaskDelay(1000); //让MPU6050提前先运行一秒获取第一笔数据
-  xTaskCreate(printTask, "print", 1024 * 8, NULL, 1, NULL);
+  xTaskCreatePinnedToCore(printTask, "print", 1024 * 8, NULL, 1, NULL,1);
+  vTaskDelete(NULL); //删除setup函数创建的空闲任务
 }
 
 void loop() {
